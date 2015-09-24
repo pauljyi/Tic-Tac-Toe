6a980c6a0337c902aecbd9efa48317c4271c8a71
Tyler <Learn02@learn02.local>
2015-09-24 16:43:03 -0700

    >=[

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,19 +1,11 @@
 $(document).ready(function() {
 
   var player = 1;
-  var board = [
-              [1,2,4],
-              [8,16,32],
-              [64,128,256]
-              ];
-  var chosenSquares = {
-                'x': [[],[],[]],
-                'o': [[],[],[]]
-                      };
+  var count = 0;
+  var x = [];
+  var o = [];
   var win = [7,56,73,84,146,273,292,448];
-  var score = {"x":0, "o":0};
-
-
+  // var win = [[1,2,4],[8,16,32],[64,128,256],[1,8,64],[2,16,128],[4,32,256],[1,16,256],[4,16,64]];
 
   function isEven(value) {
     if (value%2 == 0)
@@ -22,71 +14,74 @@ $(document).ready(function() {
     return false;
   }
 
-
   $('td').one('mousemove', function() {
-    var mouseID = '#'+this.id;
-
-    $(mouseID).one('click', function() {
+    var mouseID = this.id;
+    $('#'+mouseID).one('click', function() {
 
       if(isEven(player)){
         $("img", this).attr('src', 'images/o.jpg');
+
+        // Adds value to array
+        var cc = parseInt($('td#'+mouseID).attr('class'));
+        o.push(cc);
+
+        console.log(isWinner(o));
+
         player = 1;
-
-        var val = $(this).attr('class');
-        console.log(val);
-
+        count++;
 
       } else {
         $("img", this).attr('src', 'images/x.jpg');
+
+        //Adds value to array
+        var cc = parseInt($('td#'+mouseID).attr('class'));
+        x.push(cc);
+
+        console.log(isWinner(x));
+
         player = 2;
-
-        var val = $(this).attr('class');
-        console.log(val);
-
+        count++;
       };
     });
-
   });
 
+  var isWinner = function(array){
+    $.each(win, function(i,val){
+      if (array[0]+array[1]+array[2] === val) {
 
+        } else if
+        (array[0]+array[1]+array[3] === val) {
 
+        } else if
+        (array[0]+array[1]+array[4] === val) {
 
+        } else if
+        (array[0]+array[2]+array[3] === val) {
 
+        } else if
+        (array[0]+array[2]+array[4] === val) {
 
+        } else if
+        (array[0]+array[3]+array[4] === val) {
 
+        } else if
+        (array[1]+array[2]+array[3] === val) {
 
-  // var availSquare = [0,1,2,3,4,5,6,7,8];
-  // var ranSquare = availSquare [Math.floor(Math.random()*availSquare,length)];
-  //
-  // if (ranSquare != );
+        } else if
+        (array[1]+array[2]+array[4] === val) {
 
+        } else if
+        (array[2]+array[3]+array[4] === val) {
 
-  //
-  // if( ){
-  //
-  // }
-  // alert("You Win");
-  //
-  // win = function(score) {
-  //   for ( var i=0; i < win.length; i+=1) {
-  //     if ((win[i] & score) === win[i]) {
-  //
-  //     }
-  //
-  //   }
-  // }
-  //
-  //
-  //
+        };
 
+      });
 
-
-
-
-
-
-
-
-
-
+    };
 });
+
+// function reset(grid){
+//   grid.find('td').each(function() {
+//     $(this).removeClass('class').removeClass('');
+//   });
+// }
