9fe2fdf1711338b2fc676bd6cbe0126890c430f2
Tyler <Learn02@learn02.local>
2015-09-25 16:50:08 -0700

    It's Friday

---
+++ js/intro.js
@@ -0,0 +1,4 @@
+
+
+player1 = document.getElementById('player1').value;
+player2 = document.getElementById('player2').value;
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,20 +1,22 @@
+
 $(document).ready(function() {
 
-  var moves = 0,
+  var player1 = "",
+      player2 = "",
+      moves = 0,
       SIZE = 3,
       turn = "X",
-      score = {
-            "X": 0,
-            "O": 0
-              },
+      score = {"X": 0, "O": 0},
       wins = [7,56,73,84,146,273,292,448];
 
   $('td').one('mousemove', function(){
     var mouseID = this.id;
     $('#'+mouseID).one('click', function(){
+
       if(turn === "O") {
         $("img", this).attr('src', 'images/o.jpg');
         set(mouseID);
+
       } else {
         $("img", this).attr('src', 'images/x.jpg');
         set(mouseID);
@@ -28,11 +30,16 @@ $(document).ready(function() {
     score[turn] += parseInt($('td#'+mouseID).attr('class'));
     console.log(score[turn]);
     if (win(score[turn])) {
-          alert(turn + " wins!");
-          location.reload(true);
+        if(turn == "X"){
+          $('#p1win').css('display','inline');
+        }else{
+          $('#p2win').css('display','inline');
+        }
+          // location.reload(true);
       } else if (moves === SIZE * SIZE){
-          alert("Cat\u2019s game!");
-          location.reload(true);
+        $('#p1win').replaceWith('<h1>LOSER</h1>')
+        $('#p2win').replaceWith('<h1>LOSER</h1>')
+       // location.reload(true);
       } else {
           turn = turn === "X" ? "O" : "X";
       }
@@ -46,4 +53,15 @@ $(document).ready(function() {
     }
     return false;
   }
+
+  $('#submit').click(function(){
+    player1 = $('input#player1').val();
+    player2 = $('input#player2').val();
+    $('h1#p1name').replaceWith('<h1>'+player1+'</h1>');
+    $('h1#p2name').replaceWith('<h1>'+player2+'</h1>');
+    $('.player1name').css('visibility','visible');
+    $('.player2name').css('visibility','visible');
+    $('div#form').css('display','none');
+    $('table').css('visibility','visible');
+  });
 });
