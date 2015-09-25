600d57ecab50966b39b52e5bd09a71f4fee76d07
TylerLHenderson <tehendo@gmail.com>
2015-09-24 23:27:39 -0700

    =]]]]]]]]]

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,87 +1,49 @@
 $(document).ready(function() {
 
-  var player = 1;
-  var count = 0;
-  var x = [];
-  var o = [];
-  var win = [7,56,73,84,146,273,292,448];
-  // var win = [[1,2,4],[8,16,32],[64,128,256],[1,8,64],[2,16,128],[4,32,256],[1,16,256],[4,16,64]];
+  var moves = 0,
+      SIZE = 3,
+      turn = "X",
+      score = {
+            "X": 0,
+            "O": 0
+              },
+      wins = [7,56,73,84,146,273,292,448];
 
-  function isEven(value) {
-    if (value%2 == 0)
-    return true;
-    else
-    return false;
-  }
-
-  $('td').one('mousemove', function() {
+  $('td').one('mousemove', function(){
     var mouseID = this.id;
-    $('#'+mouseID).one('click', function() {
-
-      if(isEven(player)){
+    $('#'+mouseID).one('click', function(){
+      if(turn === "O") {
         $("img", this).attr('src', 'images/o.jpg');
-
-        // Adds value to array
-        var cc = parseInt($('td#'+mouseID).attr('class'));
-        o.push(cc);
-
-        console.log(isWinner(o));
-
-        player = 1;
-        count++;
-
+        set(mouseID);
       } else {
         $("img", this).attr('src', 'images/x.jpg');
-
-        //Adds value to array
-        var cc = parseInt($('td#'+mouseID).attr('class'));
-        x.push(cc);
-
-        console.log(isWinner(x));
-
-        player = 2;
-        count++;
+        set(mouseID);
       };
     });
   });
 
-  var isWinner = function(array){
-    $.each(win, function(i,val){
-      if (array[0]+array[1]+array[2] === val) {
+  function set (mouseID){
+    moves += 1;
+    //adds score to class
+    score[turn] += parseInt($('td#'+mouseID).attr('class'));
+    console.log(score[turn]);
+    if (win(score[turn])) {
+          alert(turn + " wins!");
+          location.reload(true);
+      } else if (moves === SIZE * SIZE){
+          alert("Cat\u2019s game!");
+          location.reload(true);
+      } else {
+          turn = turn === "X" ? "O" : "X";
+      }
+  }
 
-        } else if
-        (array[0]+array[1]+array[3] === val) {
-
-        } else if
-        (array[0]+array[1]+array[4] === val) {
-
-        } else if
-        (array[0]+array[2]+array[3] === val) {
-
-        } else if
-        (array[0]+array[2]+array[4] === val) {
-
-        } else if
-        (array[0]+array[3]+array[4] === val) {
-
-        } else if
-        (array[1]+array[2]+array[3] === val) {
-
-        } else if
-        (array[1]+array[2]+array[4] === val) {
-
-        } else if
-        (array[2]+array[3]+array[4] === val) {
-
-        };
-
-      });
-
-    };
+  function win (score) {
+    for (var i = 0; i < wins.length; i += 1){
+        if ((wins[i] & score) === wins[i]){
+            return true;
+        }
+    }
+    return false;
+  }
 });
-
-// function reset(grid){
-//   grid.find('td').each(function() {
-//     $(this).removeClass('class').removeClass('');
-//   });
-// }
